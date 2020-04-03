930291620a63f4f588e0f656cfd9272c98d64973
dymanoid <9433345+dymanoid@users.noreply.github.com>
2020-04-03 22:35:59 +0200

    Create the module initialization callback

---
+++ src/ConflictSolver/ModuleInitializer.cs
@@ -0,0 +1,17 @@
+// <copyright file="ModuleInitializer.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+namespace ConflictSolver
+{
+    /// <summary>
+    /// A static class that holds a static method which will be called by the module initializer.
+    /// </summary>
+    internal static class ModuleInitializer
+    {
+        /// <summary>
+        /// Initializes the conflict solver engine.
+        /// </summary>
+        public static void Initialize() => GameConnection.GetInstance<Engine>().Run();
+    }
+}
