be22b5088bd0a6dde77e83183ab130134ef66648
dymanoid <9433345+dymanoid@users.noreply.github.com>
2020-04-03 23:09:19 +0200

    Implement assembly check service

---
+++ src/ConflictSolver/AssemblyCheck.cs
@@ -0,0 +1,55 @@
+// <copyright file="AssemblyCheck.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Xml.Serialization;
+using ColossalFramework.Plugins;
+using Harmony;
+using UnityEngine;
+
+namespace ConflictSolver
+{
+    /// <summary>
+    /// A helper class for determining the assembly categories.
+    /// </summary>
+    internal sealed class AssemblyCheck : IAssemblyCheck
+    {
+        private static readonly string HarmonyAssemblyName = typeof(HarmonyInstance).Assembly.GetName().Name;
+
+        // Using a list instead of hash set because we only have 6 entries, hash set will not be faster
+        private readonly List<Assembly> _gameAssemblies = new List<Assembly>()
+        {
+            typeof(Type).Assembly,
+            typeof(XmlSerializer).Assembly,
+            typeof(MonoBehaviour).Assembly,
+            typeof(PluginManager).Assembly,
+            typeof(SimulationManager).Assembly,
+        };
+
+        private readonly List<Assembly> _systemAssemblies = new List<Assembly>()
+        {
+            typeof(Type).Assembly,
+            typeof(XmlSerializer).Assembly,
+        };
+
+        /// <inheritdoc/>
+        public bool IsHarmony(Assembly assembly) =>
+            assembly.GetName().Name == HarmonyAssemblyName; // Checking via name because there could be multiple assembly instances
+
+        /// <inheritdoc/>
+        public bool IsUserModAssembly(Assembly assembly) => !IsHarmony(assembly) && !_gameAssemblies.Contains(assembly);
+
+        /// <inheritdoc/>
+        public bool IsGameAssembly(Assembly assembly) => _gameAssemblies.Contains(assembly);
+
+        /// <inheritdoc/>
+        public bool IsSystemAssembly(Assembly assembly) => _systemAssemblies.Contains(assembly);
+
+        /// <inheritdoc/>
+        public bool IsDynamic(Assembly assembly) => assembly.ManifestModule is ModuleBuilder builder && builder.IsTransient();
+    }
+}
---
+++ src/ConflictSolver/IAssemblyCheck.cs
@@ -0,0 +1,54 @@
+// <copyright file="IAssemblyCheck.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+
+namespace ConflictSolver
+{
+    /// <summary>
+    /// An interface for a helper service for determining the assembly categories.
+    /// </summary>
+    internal interface IAssemblyCheck
+    {
+        /// <summary>
+        /// Determines whether the specified <paramref name="assembly"/> is an original game assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly to check. Cannot be null.</param>
+        /// <returns><c>true</c> if the specified <paramref name="assembly"/> is a game assembly;
+        /// otherwise, <c>false</c>.</returns>
+        bool IsGameAssembly(Assembly assembly);
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="assembly"/> is a system assembly, like mscorlib.
+        /// </summary>
+        /// <param name="assembly">The assembly to check. Cannot be null.</param>
+        /// <returns><c>true</c> if the specified <paramref name="assembly"/> is a system assembly;
+        /// otherwise, false.</returns>
+        bool IsSystemAssembly(Assembly assembly);
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="assembly"/> is a Harmony Library assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly to check. Cannot be null.</param>
+        /// <returns><c>true</c> if the specified <paramref name="assembly"/> is a Harmony Library assembly;
+        /// otherwise, <c>false</c>.</returns>
+        bool IsHarmony(Assembly assembly);
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="assembly"/> is a user mod assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly to check. Cannot be null.</param>
+        /// <returns><c>true</c> if the specified <paramref name="assembly"/> is a user mod assembly;
+        /// <c>false</c> if the <paramref name="assembly"/> is a game assembly.</returns>
+        bool IsUserModAssembly(Assembly assembly);
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="assembly"/> is a dynamically generated assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly to check. Cannot be null.</param>
+        /// <returns><c>true</c> if the specified <paramref name="assembly"/> is a dynamically generated assembly;
+        /// otherwise, <c>false</c>.</returns>
+        bool IsDynamic(Assembly assembly);
+    }
+}
