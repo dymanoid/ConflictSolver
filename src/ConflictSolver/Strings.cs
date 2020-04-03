67cf9a02878b46266680f8ed2665bbfc9dded231
dymanoid <9433345+dymanoid@users.noreply.github.com>
2020-04-03 22:33:11 +0200

    Create container for string constants

---
+++ src/ConflictSolver/Strings.cs
@@ -0,0 +1,22 @@
+// <copyright file="Strings.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+namespace ConflictSolver
+{
+    /// <summary>
+    /// A class containing the commonly used constant strings.
+    /// </summary>
+    internal static class Strings
+    {
+        /// <summary>
+        /// The name of the mod.
+        /// </summary>
+        public const string ModName = "Mod Conflict Solver";
+
+        /// <summary>
+        /// A prefix string for a debug log entry.
+        /// </summary>
+        public const string DebugLogPrefix = "The 'ConflictSolver' mod";
+    }
+}
