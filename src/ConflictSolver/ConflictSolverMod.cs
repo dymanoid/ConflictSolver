80c6513f896499ca838da6ecaa3c18b4b2ffea79
dymanoid <9433345+dymanoid@users.noreply.github.com>
2020-04-04 09:09:02 +0200

    Log monitored data on city unloading

--- src/ConflictSolver/ConflictSolverMod.cs
+++ src/ConflictSolver/ConflictSolverMod.cs
@@ -2,6 +2,8 @@
 // Copyright (c) dymanoid. All rights reserved.
 // </copyright>
 
+using System.Text;
+using ConflictSolver.Results;
 using ICities;
 
 namespace ConflictSolver
@@ -26,5 +28,51 @@ namespace ConflictSolver
         /// </summary>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822", Justification = "Will be called by the game")]
         public void OnDisabled() => GameConnection.DestroyInstance<Engine>(e => e.Shutdown());
+
+        /// <summary>
+        /// Called by the game when a map or a city is about to be unloaded.
+        /// </summary>
+        public override void OnLevelUnloading()
+        {
+            var dataSource = Storage.GetData();
+            var modInfoProvider = new ModInfoProvider();
+            var dataView = new DataView(dataSource, modInfoProvider);
+            dataView.Prepare();
+
+            var sb = new StringBuilder();
+
+            sb.Append(Strings.DebugLogPrefix)
+                .AppendLine(" dumps the conflicting mods report...");
+
+            foreach (string mod in dataView.GetMonitoredModNames())
+            {
+                sb.Append("Monitored mod '")
+                    .Append(mod)
+                    .AppendLine("'");
+
+                foreach (string queriedType in dataView.GetQueriedMembers(mod))
+                {
+                    sb.Append(" -> Queried: ")
+                        .AppendLine(queriedType);
+                }
+
+                foreach (var conflict in dataView.GetConflicts(mod))
+                {
+                    sb.Append(" ** Possible conflict with mod '")
+                        .Append(conflict.ModName)
+                        .AppendLine("'");
+
+                    foreach (string member in conflict.MemberNames)
+                    {
+                        sb.Append("    - ")
+                            .AppendLine(member);
+                    }
+                }
+            }
+
+            sb.AppendLine("Report completed.");
+
+            UnityEngine.Debug.Log(sb.ToString());
+        }
     }
 }
